fac2cf3862842a4ee51a5b0506c598f207fb8984
Tanuj Gupta <tanujgupta1872003@gmail.com>
2025-09-16 18:51:09 +0530

    Practise Set 3

---
+++ Basic Practise Set/PractiseSet-3.py
@@ -0,0 +1,22 @@
+# Program 1
+name = input("Enter your name: ")
+print(f"Good Afternoon, {name}")
+
+# # Program 2
+letter = ''' Dear <|Name|>,
+You are selected!
+<|Date|> '''
+
+print(letter.replace("<|Name|>","Tanuj").replace("<|Date|>","16 September 2025"))
+
+# Program 3
+string = "Hey! I  am a Web Developer"
+print(string.find("  "))
+
+# Program 4
+print(string.replace("  "," "))
+
+# Program 5
+letter = "Dear User,\n\tThis python course is nice.\nThanks!"
+print(letter)
+
