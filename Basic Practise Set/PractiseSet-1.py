5ec4276c1eb0ddb5679e9ce551ef1e94d18957e5
Tanuj Gupta <tanujgupta1872003@gmail.com>
2025-09-16 15:24:46 +0530

    Practise Set 1

---
+++ Basic Practise Set/PractiseSet-1.py
@@ -0,0 +1,30 @@
+# Program 1
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+result = num1 + num2
+print("Sum of two Numbers: ",result)
+
+# Program 2
+a = 15
+b = 10
+print("Remainder when a is divided by b is",a%b)
+
+# Program 3
+c = input("Enter the value of c ")
+print(type(c))
+
+# Program 4
+a = 34
+b = 80
+print("a is greater than b is",a>b)
+
+# Program 5
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+Avg = (num1 + num2)/2
+print("Average of two Numbers: ",Avg)
+
+# Program 6
+num = int(input("Enter a number: "))
+sqaure = num * num
+print("Square is",sqaure)
