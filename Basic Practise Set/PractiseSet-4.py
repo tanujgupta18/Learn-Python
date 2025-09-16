29a8e2741c994917f31d468bcdc6399fa907928d
Tanuj Gupta <tanujgupta1872003@gmail.com>
2025-09-16 20:51:15 +0530

    Practise Set 4

---
+++ Basic Practise Set/PractiseSet-4.py
@@ -0,0 +1,35 @@
+# Program 1
+fruits = []
+f1 = input("Enter fruit Name: ")
+fruits.append(f1)
+print(fruits)
+
+# Program 2
+marks = []
+
+m1 = int(input("Enter marks here: "))
+m2 = int(input("Enter marks here: "))
+m3 = int(input("Enter marks here: "))
+m4 = int(input("Enter marks here: "))
+m5 = int(input("Enter marks here: "))
+m6 = int(input("Enter marks here: "))
+
+marks.append(m1)
+marks.append(m2)
+marks.append(m3)
+marks.append(m4)
+marks.append(m5)
+marks.append(m6)
+print(marks)
+
+# Program 3
+a = (1,5,4)
+a[2] = "Hello"
+
+# Program 4
+list = [1,2,5,7]
+print(sum(list))
+
+# Program 5
+a = (7, 0, 8, 0, 0, 9)
+print(a.count(0))
